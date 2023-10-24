5b725a1d127a9fcf3c0444ee5a05280df13a2144
jiaxin96 <572673807@qq.com>
2023-10-25 04:14:37 +0800

    add link65

---
+++ keyboards/yandrstudio/wired/link65_alt/config.h
@@ -0,0 +1,42 @@
+// Copyright 2023 Y&R-Biu (@jiaxin96)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xAA96
+#define PRODUCT_ID      0xAAF8
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    "Y&R"
+#define PRODUCT         "Link65"
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+//                        0    1    2    3   4
+#define MATRIX_COL_PINS { }
+#define MATRIX_ROW_PINS { B0, B1, B2, B10, B11 }
+
+#define NUM_OF_74HC595 2
+#define COL_F2L_FOR_595 TRUE
+
+#define SR_74HC595_ZERO_ONEP 0x00
+#define SR_74HC595_ONES_ONEP 0xFF
+
+//                              a    b     c     d     e     f     g     h
+#define SR_74HC595_LINE_MASK { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 }
+//                          c0    c1    c2    c3    c4    c5    c6    c7    c8    c9    c10,  c11   c12   c13   c14
+#define MATRIX_OF_74HC595 {{0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},\
+                           {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01}}
+
+#define SPI_SCK_PIN_OF_595 A5
+#define SPI_MOSI_PIN_OF_595 A7
+#define SPI_74HC595_CS A4
+
+#define TAP_CODE_DELAY 15
+
+#define TAP_HOLD_CAPS_DELAY 250
---
+++ keyboards/yandrstudio/wired/link65_alt/readme.md
@@ -0,0 +1,16 @@
+link65_alt
+===
+
+A 65% keyboard, APM32F103, 16M
+
+Keyboard Maintainer: https://github.com/jiaxin96
+
+Hardware Supported: link65_alt
+
+Hardware Availability: https://github.com/Oh-My-Mechanical-Keyboard 
+
+Make example for this keyboard (after setting up your build environment):
+
+    make yandrstudio/wired/link65_alt:via
+
+See [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) then the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information.
