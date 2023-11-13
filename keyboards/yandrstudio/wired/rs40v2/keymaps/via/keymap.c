b11daa2d74e441374fd171ef433636fc95dfe570
superYoungMonk <monk@yrkb.cc>
2023-11-14 00:30:48 +0800

    调整rs40v2固件矩阵，调整对应按键值

--- keyboards/yandrstudio/wired/rs40v2/keymaps/via/keymap.c
+++ keyboards/yandrstudio/wired/rs40v2/keymaps/via/keymap.c
@@ -5,7 +5,7 @@
 /*
     K000, K001,  K002,  K003,  K004,  K005,  K006, K007, K008, K009, K010, K011, \
     K100, K101,  K102,  K103,  K104,  K105,  K106, K107, K108, K109,       K111, \
-    K200, K201,  K202,  K203,  K204,  K205,  K206, K207, K208, K209, K210,       \
+    K200, K201,  K202,  K203,  K204,  K205,  K206, K207, K208, K209, K110,       \
     K300, K301,  K302,         K304,  K305,        K307,       K309, K310        \
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // 0     1                                          6        7        8        9        10       11
         KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_CAPS, KC_A,    KC_S,    KC_D,   KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_RSFT, KC_UP,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_UP,   KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,         KC_SPC, LT(1, KC_SPC),    KC_LEFT,          KC_DOWN, KC_RGHT),
     LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,   KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
