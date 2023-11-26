0fa3824442f5334c03c3fdb650fd8ada2586ca72
jiaxin96 <572673807@qq.com>
2023-11-26 21:07:14 +0800

    swk

--- keyboards/yandrstudio/wired/swk60/config.h
+++ keyboards/yandrstudio/wired/swk60/config.h
@@ -14,8 +14,8 @@
 #define MATRIX_COLS 18
 #define MATRIX_ROWS 5
 
-//                        0   1   2   3   4   5   6   7   8    9    10   11  12  13   14  15  16   17
-#define MATRIX_COL_PINS { A3, A4, A5, A6, A7, B0, B1, B2, B10, B11, B15, A8, A9, A10, B5, B4, A15, B3 }
+//                        0   1   2   3   4   5   6   7   8    9    10   11  12  13   14  15  16  17
+#define MATRIX_COL_PINS { A3, A4, A5, A6, A7, B0, B1, B2, B10, B11, B15, A8, A9, A10, B3, B4, B5, A15 }
 #define MATRIX_ROW_PINS { B6, B7, A0, A1, A2}
 
 /* COL2ROW or ROW2COL */
