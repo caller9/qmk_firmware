57dbaf62c1ffebcff87049c02d2323670dbf1366
jiaxin96 <572673807@qq.com>
2024-01-26 20:01:30 +0800

    更新t60e 热拔插 错版

--- keyboards/yandrstudio/wired/t60e/config.h
+++ keyboards/yandrstudio/wired/t60e/config.h
@@ -18,8 +18,12 @@
 #define MATRIX_COLS 14
 
 //                         0   1    2    3    4   5   6   7   8   9   10  11  12  13
-#define MATRIX_COL_PINS {  B2, B10, B11, A15, B3, B4, B5, B7, B8, B9, A3, A4, A5, A6}
-#define MATRIX_ROW_PINS {  A2, A1,  A0,  B6,  A7 }
+// #define MATRIX_COL_PINS {  B2, B10, B11, A15, B3, B4, B5, B7, B8, B9, A3, A4, A5, A6}
+// #define MATRIX_ROW_PINS {  A2, A1,  A0,  B6,  A7 }
+
+// V1-DISMAKE
+#define MATRIX_COL_PINS {  B2, B10, B11, A15, B3, B4, B5, B6, B8, B9, A3, A4, A5, A6}
+#define MATRIX_ROW_PINS {  A2, A1,  A0,  B7,  B0 }
 
 #define LED_CAPS_LOCK_PIN B1
 #define LED_PIN_ON_STATE  1
