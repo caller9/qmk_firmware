ba2af5c63971a83be4087afa86f9fab3d402bc97
jiaxin96 <572673807@qq.com>
2024-02-28 22:18:25 +0800

    更新ec60

---
+++ keyboards/yandrstudio/wired/t60e_ec/rules.mk
@@ -0,0 +1,21 @@
+# MCU name
+MCU = STM32F401
+
+# Bootloader selection
+# BOOTLOADER = stm32-dfu
+BOOTLOADER = tinyuf2
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
+KEYBOARD_SHARED_EP = no   # Free up some extra endpoints - needed if console+mouse+extra
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+
+CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
+QUANTUM_SRC += matrix_ec.c
