c3b6eaa4c16516831e566e0df7d33348f3fca1b9
jiaxin96 <572673807@qq.com>
2024-08-05 03:47:26 +0800

    更新ving

---
+++ keyboards/yandrstudio/dev/ving/rev1/stm32/config.h
@@ -0,0 +1,11 @@
+// Copyright 2018-2022 Nick Brassel (@tzarc)
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+// RGB configuration
+#define WS2812_PWM_DRIVER PWMD5
+#define WS2812_PWM_CHANNEL 4
+#define WS2812_PWM_PAL_MODE 2
+#define WS2812_DMA_STREAM STM32_DMA1_STREAM0
+#define WS2812_DMA_CHANNEL 6
+
---
+++ keyboards/yandrstudio/dev/ving/rules.mk
@@ -0,0 +1,5 @@
+ALLOW_WARNINGS = yes
+
+RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+SRC += analog.c
+# DEBUG_MATRIX_SCAN_RATE_ENABLE = yes
